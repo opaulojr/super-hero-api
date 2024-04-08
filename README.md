4291bf85a9c74c62626c49381d11c5c8d45b35a0
paulojr.hs <paulojr.hs@gmail.com>
2024-04-08 01:23:58 -0300

    update-project

--- README.md
+++ README.md
@@ -1,11 +1,39 @@
-# Projeto Super Hero API
+# Project Super Hero API
 
-Este é o meu 12º projeto durante minha jornada na Trybe!
+[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
 
-## Estrutura do Repositório
+## Preview
 
-- A pasta `src` contém todo o código;
+![Preview of Super Hero API](./super-hero-api.png)
 
-## Funcionalidades Implementadas
+## About
 
-## :construction: Em construção ! :construction:
+This is my 12th project during my journey at Trybe!
+
+[Super Hero API](https://akabab.github.io/superhero-api/api/) is responsible for displaying heroes randomly and providing their information and trivia. This is my first time consuming a real API, handling its responses, data, and possible errors; displaying to the user the most important and relevant information!
+
+## Repository Structure
+
+- The `src` folder contains all the code
+
+## Implemented Features
+
+### API Consumption
+
+  - The application consumes the [Super Hero API](https://akabab.github.io/superhero-api/api/) and uses the .JSON response file to display:
+
+    - Hero's image
+    - Hero's name
+    - Their biography
+
+  - The consumed endpoint is `'https://akabab.github.io/superhero-api/api/id/number.json'` where **'number'** is a number between 1 and 562 (total number of heroes in the API). There is a function responsible for generating random numbers so that they are dynamically passed to the endpoint
+
+  - After the random number is generated, it is stored in an array, thus creating a kind of history so that later if the function responsible for generating a random number generates a number that has already been generated previously, it repeats the generation process, thus avoiding the repetition of displayed heroes
+
+---
+
+### Swal Library
+
+  - [Swal](https://sweetalert2.github.io/) is an abbreviation used to refer to SweetAlert, a library that facilitates the creation of highly customizable and visually appealing dialog modals. In this project, it was used to display a dialog modal of the heroes' biography
+
+---
--- src/main.js
+++ src/main.js
@@ -8,24 +8,76 @@ const sortBtn = document.getElementById('sort-button');
 const API_URL = 'https://akabab.github.io/superhero-api/api';
 
 const MAX_HEROES = 562;
+const MAX_HISTORY = 100;
 
-const randomId = () => Math.floor(Math.random() * MAX_HEROES);
+const idHistory = [];
+
+const randomId = () => {
+  let id = Math.floor(Math.random() * MAX_HEROES);
+  while (idHistory.includes(id)) {
+    id = Math.floor(Math.random() * MAX_HEROES);
+  }
+
+  return id;
+};
+
+const addToHistory = (id) => {
+  idHistory.push(id);
+
+  if (idHistory.length > MAX_HISTORY) idHistory.shift();
+};
+
+const fetchHeroData = () => {
+  const id = randomId();
+  addToHistory(id);
+
+  fetch(`${API_URL}/id/${id}.json`)
+    .then((result) => {
+      if (!result.ok) throw new Error('Network response was not ok');
+      return result.json();
+    })
+    .then((data) => {
+      heroImg.src = data.images.lg;
+      heroName.innerHTML = data.name;
+      heroName.addEventListener('click', () => {
+        Swal.fire({
+          title: data.name,
+          html: `
+            <h3>Full Name</h3>
+            <p>${data.biography.fullName}</p>
+
+            <h3>Alter Egos</h3>
+            <p>${data.biography.alterEgos}</p>
+
+            <h3>Aliases</h3>
+            <p>${data.biography.aliases.join(', ')}</p>
+
+            <h3>Place of Birth</h3>
+            <p>${data.biography.placeOfBirth}</p>
+
+            <h3>First Appearance</h3>
+            <p>${data.biography.firstAppearance}</p>
+
+            <h3>Publisher</h3>
+            <p>${data.biography.publisher}</p>
+
+            <h3>Alignment</h3>
+            <p>${data.biography.alignment}</p>
+          `,
+          icon: 'info',
+          confirmButtonText: 'OK',
+          background: '#242424',
+          color: '#fff',
+        });
+      });
+    })
+    .catch((error) => {
+      console.error('Hero not found:', error);
+      fetchHeroData();
+    });
+};
 
 sortBtn.addEventListener('click', (event) => {
   event.preventDefault();
-
-  const id = randomId();
-
-  fetch(`${API_URL}/id/${id}.json`)
-    .then((result) => result.json())
-    .then((data) => {
-      heroImg.src = data.images.md;
-      heroName.innerHTML = data.name;
-    })
-    .catch((error) => Swal.fire({
-      title: 'Hero not found',
-      text: error.message,
-      icon: 'error',
-      confirmButtonText: 'OK',
-    }));
+  fetchHeroData();
 });
